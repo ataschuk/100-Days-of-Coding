859410fd381f6b109acdef7eaf6a4507c4fbcbbb
ataschuk <alextaschuk@gmail.com>
2023-02-03 12:52:12 -0800

    Day 2: continued working on creating checkered pattern.

--- snake/turtlePractice.py
+++ snake/turtlePractice.py
@@ -5,37 +5,85 @@ from turtle import *
 WIDTH, HEIGHT = 680,680
 
 screen = Screen() # create a screen
-screen.bgcolor('green')
+screen.screensize(WIDTH,HEIGHT) # set width and height of screen to 680 x 680
 screen.title("Turtle Practice") # set the title
-screen.setup(HEIGHT, WIDTH)
 
 
 
-pen = Turtle() # create new turtle 
-pen.shape('square') # set turtle shape to a square
-pen.color('dark green') # set the color to dark green
+tile1 = Turtle() # create new turtle 
+tile1.shape('square') # set turtle shape to a square
+tile1.speed(0) # this boy fast as hell
+tile1.penup() #hide turtle lines
 
-pen.speed(0) # this boy fast as hell
-pen.penup()
+tile1.goto(0,0)
+tile1.stamp()
+tile1.goto(-331,328) # top left corner of screen
+tile1.stamp()
+tile1.goto(-331, -328)
+tile1.stamp()
+tile1.goto(331, 328)
+tile1.stamp()
+tile1.goto(331,-328)
+tile1.stamp()
+tile1.setheading(0) # face turtle east
+'''
+switchColor = False
+for i in range(34): # function that draws a square
+    if i % 2 == 0:
+        tile1.color('green')
+        tile1.stamp()
+        tile1.forward(21)
+    if i % 2 != 0:
+        tile1.forward(21)
+    if tile1.xcor() > 299:
+        tile1.right(90)
+        tile1.stamp()
+        tile1.forward(21)
+        tile1.right(90)
+        switchColor == True
+    elif tile1.xcor() < -299:
+        tile1.left(90)
+        tile1.stamp()
+        tile1.forward(21)
+        tile1.left(90)
+        switchColor = True
+    if switchColor == True:
+        tile1.color('dark green')
 
-pen.goto(-328,329) # put the turtle in the top left of the screen
-pen.setheading(0)
+tile2 = Turtle()
+tile2.shape('square')
+tile2.speed(0) # this boy fast as hell
+tile2.penup() #hide turtle lines
+tile2.goto(-331,328) # top left corner of screen
+tile2.setheading(0) # face turtle east
 
+for j in range(34): # function that draws a square
+    if j % 2 != 0:
+        tile2.color('dark green')
+        tile2.stamp()
+        tile2.forward(21)
+    if j % 2 == 0:
+        tile2.forward(21)
+    if tile2.xcor() > 300:
+        tile2.right(90)
+        tile2.stamp()
+        tile2.forward(21)
+        tile2.right(90)
+        switchColor == True
+    elif tile2.xcor() < -300:
+        tile2.left(90)
+        tile2.stamp()
+        tile2.forward(21)
+        tile2.left(90)
+        switchColor = True
+    if switchColor == True:
+        tile2.color('green')
+'''
 
-
-
-for i in range(68): # function that draws a square
-    pen.stamp()
-    pen.forward(42) # moves the turtle 100 pixels forward and draws a line
-    if i == 17:
-        pen.right(90)
-        pen.right(90)
-        continue
- 
     
 
 
 
 
 
-turtle.done() # keeps the screen open for more than 1 millisecond
+turtle.done() # keeps the screen otile1 for more than 1 millisecond
