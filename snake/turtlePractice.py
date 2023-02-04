cbb9d0175a348ccb275f4b5aa48ca1f7c04b2db4
ataschuk <alextaschuk@gmail.com>
2023-02-03 16:10:48 -0800

    added comments to turtlePractice file.

--- snake/turtlePractice.py
+++ snake/turtlePractice.py
@@ -5,38 +5,32 @@ from turtle import *
 WIDTH, HEIGHT = 680,620
 
 screen = Screen() # create a screen
-screen.setup(WIDTH + 4, HEIGHT + 8)
-screen.setworldcoordinates(0, 0, WIDTH,HEIGHT)
-# screen.screensize(WIDTH,HEIGHT) # set width and height of screen to 680 x 680
-screen.title("Turtle Practice") # set the title
-
-
+screen.setup(WIDTH + 4, HEIGHT + 8) # setsthe amount of area that the turtle can move. +4 and +8 are to take into account window border and title bar
+screen.setworldcoordinates(0, 0, WIDTH,HEIGHT) # sets up a user-defined coordiate system with 0,0 in the bottom right corner and the top right (x,y) as 
+                                               # Width and Height
+screen.title("Turtle Practice") # sets the title on the window
 
 tile1 = Turtle() # create new turtle 
-tile1.shapesize(3,3,0)
+tile1.shapesize(3,3,0) # set the size of the turtle to 60 x 60 pixels
 tile1.shape('square') # set turtle shape to a square
 tile1.speed(0) # this boy fast as hell
 tile1.penup() #hide turtle lines
 tile1.color('green') #turtle color is green
 tile1.setpos(30, 585) #set turtle position to top right corner
-tile1.setheading(0) # face turtle east
-tile1._tracer(0,0)
+tile1._tracer(0,0) # instantly create the drawing without any animation
 
-
-
-
-for i in range(110): # function that draws a square
-    if tile1.xcor() > 661:
+for i in range(110): # function that draws a  green square
+    if tile1.xcor() > 661: # if the turtle gets past a certain coordinate, have it go to the next line down and do a 180 degree turn
         tile1.right(90)
         tile1.forward(61)
         tile1.right(90)
         tile1.forward(61)
-    if tile1.xcor() < 30:
+    if tile1.xcor() < 30: # if the turtle gets past a certain coordinate, have it go to the next line down and do a 180 degree turn
         tile1.left(90)
         tile1.forward(61)
         tile1.left(90)
         tile1.forward(61)
-    if i % 2 == 0:
+    if i % 2 == 0: # if the turtle is on an "even" square, stamp the square in the turtle's location, then move forward. otherwise, just move forward
         tile1.stamp()
         tile1.forward(61)
     else:
@@ -44,35 +38,31 @@ for i in range(110): # function that draws a square
 
 
 tile2 = Turtle() # create new turtle 
-tile2.shapesize(3,3,0)
+tile2.shapesize(3,3,0) # set the size of the turtle to 60 x 60 pixels
 tile2.shape('square') # set turtle shape to a square
 tile2.speed(0) # this boy fast as hell
 tile2.penup() #hide turtle lines
 tile2.color('dark green') #turtle color is green
 tile2.setpos(30, 585) #set turtle position to top right corner
 tile2.setheading(0) # face turtle east
-tile2._tracer(0.0)
+tile2._tracer(0.0) # instantly create the drawing without any animation
 
 
 for j in range(110): # function that draws a square
-    if tile2.xcor() > 661:
+    if tile2.xcor() > 661: # if the turtle gets past a certain coordinate, have it go to the next line down and do a 180 degree turn
         tile2.right(90)
         tile2.forward(61)
         tile2.right(90)
         tile2.forward(61)
-    if tile2.xcor() < 30:
+    if tile2.xcor() < 30: # if the turtle gets past a certain coordinate, have it go to the next line down and do a 180 degree turn
         tile2.left(90)
         tile2.forward(61)
         tile2.left(90)
         tile2.forward(61)
-    if j % 2 != 0:
+    if j % 2 != 0: # if the turtle is on an "odd" square, stamp the square in the turtle's location, then move forward. otherwise, just move forward
         tile2.stamp()
         tile2.forward(61)
     else:
         tile2.forward(61)
-
-
-    
-
 
 turtle.done() # keeps the screen open for more than 1 millisecond
